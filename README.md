99adedc8ba8d2a1c7fa3a044b37cbddadea62c8d
om3ga6400 <JackGA6400@gmail.com>
2025-05-01 17:35:25 +0000

    Update README.md, Create update.sh and create _miscList.txt

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CookieClicker
+
+## Todo
+
+Make updater downlaod beta
+Make updater auto scan main dir so updating _miscList.txt will be automaticly updated
+Add progress bar to updater
---
+++ update.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+xargs -a _delList.txt rm -rf -- && rm -f _delList.txt
+
+mkdir -p snd loc img
+
+curl -L -o grab.txt "http://orteil.dashnet.org/patreon/grab.php"
+curl -L -o index.html "https://orteil.dashnet.org/cookieclicker"
+
+while IFS= read -r file; do
+curl -O "https://orteil.dashnet.org/cookieclicker/$file"
+done < _miscList.txt
+
+sed -i "s|ajax('/patreon/grab.php'|ajax('grab.txt'|g" main.js
+
+curl -s https://orteil.dashnet.org/cookieclicker/snd/ | grep -oP '(?<=href=")[^"]+\.mp3' > snd/_sndList.txt
+curl -s https://orteil.dashnet.org/cookieclicker/loc/ | grep -oP '(?<=href=")[^"]+\.js' > loc/_locList.txt
+curl -s https://orteil.dashnet.org/cookieclicker/img/ | grep -oP '(?<=href=")[^"]+\.(png|jpe?g|db|gif)' > img/_imgList.txt
+
+for f in snd/_sndList.txt loc/_locList.txt img/_imgList.txt; do
+  dir="${f%/*}"
+  mkdir -p "$dir"
+  [ -f "$f" ] && xargs -a "$f" -I{} curl -f -o "$dir/{}" "https://orteil.dashnet.org/cookieclicker/$dir/{}"
+done
+
+find . -maxdepth 1 -type f ! -name '_delList.txt' ! -name '_miscList.txt' ! -iname 'readme.md' ! -iname 'update.sh' > _delList.txt
+
+for dir in snd loc img; do
+  if [ -d "$dir" ]; then
+    find "$dir" -type f ! -iname 'readme.md' ! -iname 'update.sh' ! -name '_miscList.txt' ! -name '_delList.txt' >> _delList.txt
+  fi
+done
+
+for dir in snd loc img; do
+  if [ -d "$dir" ]; then
+    echo "$dir" >> _delList.txt
+  fi
+done
