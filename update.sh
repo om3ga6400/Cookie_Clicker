6d1f45f734ac7cd31a4da8b6096f5d6b09b9d103
om3ga6400 <JackGA6400@gmail.com>
2025-05-05 15:07:56 +0000

    Better update.sh

--- update.sh
+++ update.sh
@@ -1,48 +1,29 @@
 #!/bin/sh
 
 xargs rm -f < _delList.txt
-
 mkdir -p snd loc img beta/loc beta/img beta/snd
-
 curl -L -o grab.txt "http://orteil.dashnet.org/patreon/grab.php"
 curl -L -o index.html "https://orteil.dashnet.org/cookieclicker"
-
 xargs -I {} curl -O "https://orteil.dashnet.org/cookieclicker/{}" < _miscList.txt
-
 sed -i "s|ajax('/patreon/grab.php'|ajax('grab.txt'|" main.js
-
-curl -s https://orteil.dashnet.org/cookieclicker/snd/ | grep -oP '(?<=href=")[^"]+\.mp3' > snd/_sndList.txt
-curl -s https://orteil.dashnet.org/cookieclicker/loc/ | grep -oP '(?<=href=")[^"]+\.js' > loc/_locList.txt
-curl -s https://orteil.dashnet.org/cookieclicker/img/ | grep -oP '(?<=href=")[^"]+\.(png|jpe?g|db|gif)' > img/_imgList.txt
-
-for f in snd/_sndList.txt loc/_locList.txt img/_imgList.txt; do
-  dir="${f%/*}" && mkdir -p "$dir" && xargs -a "$f" -I{} curl -f -o "$dir/{}" "https://orteil.dashnet.org/cookieclicker/$dir/{}" < "$f"
-done
-
-#Beta start
-
+curl https://orteil.dashnet.org/cookieclicker/snd/ | grep -oP '(?<=href=")[^"]+\.mp3' > snd/_sndList.txt
+curl https://orteil.dashnet.org/cookieclicker/loc/ | grep -oP '(?<=href=")[^"]+\.js' > loc/_locList.txt
+curl https://orteil.dashnet.org/cookieclicker/img/ | grep -oP '(?<=href=")[^"]+\.(png|jpe?g|db|gif)' > img/_imgList.txt
+xargs -a snd/_sndList.txt -I{} curl -f -o snd/{} "https://orteil.dashnet.org/cookieclicker/snd/{}" < snd/_sndList.txt
+xargs -a loc/_locList.txt -I{} curl -f -o loc/{} "https://orteil.dashnet.org/cookieclicker/loc/{}" < loc/_locList.txt
+xargs -a img/_imgList.txt -I{} curl -f -o img/{} "https://orteil.dashnet.org/cookieclicker/img/{}" < img/_imgList.txt
 cd beta/
-
 curl -L -o index.html "https://orteil.dashnet.org/cookieclicker/beta/"
-
 xargs -I {} curl -O "https://orteil.dashnet.org/cookieclicker/beta/{}" < ../_miscList.txt
-
 sed -i "s|ajax('/patreon/grab.php'|ajax('../grab.txt'|" main.js
-
-curl -s https://orteil.dashnet.org/cookieclicker/beta/snd/ | grep -oP '(?<=href=")[^"]+\.mp3' > snd/_sndList.txt
-curl -s https://orteil.dashnet.org/cookieclicker/beta/loc/ | grep -oP '(?<=href=")[^"]+\.js' > loc/_locList.txt
-curl -s https://orteil.dashnet.org/cookieclicker/beta/img/ | grep -oP '(?<=href=")[^"]+\.(png|jpe?g|db|gif)' > img/_imgList.txt
-
-for f in snd/_sndList.txt loc/_locList.txt img/_imgList.txt; do
-  dir="${f%/*}" && mkdir -p "$dir" && xargs -a "$f" -I{} curl -f -o "$dir/{}" "https://orteil.dashnet.org/cookieclicker/beta/$dir/{}" < "$f"
-done
-
+curl https://orteil.dashnet.org/cookieclicker/beta/snd/ | grep -oP '(?<=href=")[^"]+\.mp3' > snd/_sndList.txt
+curl https://orteil.dashnet.org/cookieclicker/beta/loc/ | grep -oP '(?<=href=")[^"]+\.js' > loc/_locList.txt
+curl https://orteil.dashnet.org/cookieclicker/beta/img/ | grep -oP '(?<=href=")[^"]+\.(png|jpe?g|db|gif)' > img/_imgList.txt
+xargs -a snd/_sndList.txt -I{} curl -f -o snd/{} "https://orteil.dashnet.org/cookieclicker/beta/snd/{}" < snd/_sndList.txt
+xargs -a loc/_locList.txt -I{} curl -f -o loc/{} "https://orteil.dashnet.org/cookieclicker/beta/loc/{}" < loc/_locList.txt
+xargs -a img/_imgList.txt -I{} curl -f -o img/{} "https://orteil.dashnet.org/cookieclicker/beta/img/{}" < img/_imgList.txt
 cd ../
-
-#Beta end
-
 find . -type f ! -path './.git/*' ! -name '_delList.txt' ! -name '_miscList.txt' ! -name 'README.md' ! -iname 'update.sh' > _delList.txt
-
 grep -Eho '["'\''][^/"'\'']+\.(js|css|html|json|xml)(\?[^"'\''"]*)?["'\'']' $(grep -Fxv '' _delList.txt) |
 grep -Ev '["'\''](https?:|\/\/)' |
 sed -E "s/^['\"]//;s/['\"].*//;s/\?.*//" |
